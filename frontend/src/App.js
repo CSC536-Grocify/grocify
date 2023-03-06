d693613d7c65fdf935933039a0a95f9a2f08e032
Eric Duong <eduong@arizona.edu>
2023-03-06 10:05:34 -0700

    host react on gcp

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import React from "react";
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000'
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 function App() {
