6488429cb77f8d0aed3f30071a748a60fe523eca
Mary <maryameskandari@email.arizona.edu>
2023-03-09 16:00:15 -0700

    I did the first page with sign up and sign in pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,44 +1,50 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import React from "react";
+import Home from './components/Home/Home'
+import Navbar from './components/Navbar/Navbar'
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_ENV && process.env.REACT_APP_ENV === 'production' ? 
     process.env.REACT_APP_REMOTE_API_URL : process.env.REACT_APP_LOCAL_API_URL,
 });
+//don't change 3lines above
 
 function App() {
-  console.log("REACT_APP_ENV=".concat(process.env.REACT_APP_ENV));
-
-  const [person, setPerson] = React.useState({});
-
-  React.useEffect(() => {
-    async function fetchData() {
-      const request = await api.get('/');
-      setPerson(request.data);
-      return request;
-    }
-    fetchData();
-  }, []);
+  // console.log("REACT_APP_ENV=".concat(process.env.REACT_APP_ENV));
+  // const [person, setPerson] = React.useState({});
+  // React.useEffect(() => {
+  //   async function fetchData() {
+  //     const request = await api.get('/');
+  //     setPerson(request.data);
+  //     return request;
+  //   }
+  //   fetchData();
+  // }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi, I am <code>{person.name}</code>. I am <code>{person.age}</code> years old.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+        <Navbar />
+        <Home />
+      </>
+    
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Hi, I am <code>{person.name}</code>. I am <code>{person.age}</code> years old.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
---
+++ frontend/src/components/Home/SignupForm.jsx
@@ -0,0 +1,209 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const SignupForm = (event) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  }
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // Handle form submission here
+  }
+
+  const handlePopup = () => {
+    const width = window.innerWidth * 0.25;
+    const height = window.innerHeight / 4;
+    const left = window.innerWidth / 2 - width / 2;
+    const top = window.innerHeight / 2 - height / 2;
+    const popup = window.open(
+      '',
+      'loginPopup',
+      `width=${width},height=${height},left=${left},top=${top}`
+    );
+    popup.document.title = 'Log In';
+    popup.document.body.style.backgroundColor = 'white';
+    popup.document.body.style.display = 'flex';
+    popup.document.body.style.justifyContent = 'center';
+    popup.document.body.style.alignItems = 'center';
+    popup.document.body.innerHTML = '<div id="login-form"></div>';
+
+    const loginForm = (
+      <LoginPopup
+        onLogin={(data) => {
+          setUsername(data.username);
+          setPassword(data.password);
+          popup.close();
+        }}
+      />
+    );
+
+    ReactDOM.render(loginForm, popup.document.getElementById('login-form'));
+  };
+  
+  return (
+    <form className="signup-form" onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input type="text" id="username" value={username} onChange={handleUsernameChange} required />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" value={password} onChange={handlePasswordChange} required />
+      </div>
+      <div>
+        <button type="submit">Sign up</button>
+      </div>
+      <div className="home-footer">
+        <p>Already have an account? <a onClick={handlePopup}>Login</a></p>
+      </div>
+    </form>
+  );
+};
+
+const LoginPopup = ({ onLogin }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    onLogin({ username, password });
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Log In</button>
+    </div>
+  );
+};
+
+export default SignupForm;
+
+
+
+
+// import React, { useState } from 'react';
+
+// const SignupForm = (event) => {
+//   const [username, setUsername] = useState('');
+//   const [password, setPassword] = useState('');
+
+//   const handleUsernameChange = (event) => {
+//     setUsername(event.target.value);
+//   }
+
+//   const handlePasswordChange = (event) => {
+//     setPassword(event.target.value);
+//   }
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     // Handle form submission here
+//   }
+//   const handleLogin = () => {
+//     if (username === 'myusername' && password === 'mypassword') {
+//       window.location.href = '/dashboard';
+//     } else {
+//       alert('Invalid username or password');
+//     }
+//   };
+//   const handlePopup = () => {
+//     const width = 400;
+//     const height = 400;
+//     const left = window.innerWidth / 2 - width / 2;
+//     const top = window.innerHeight / 2 - height / 2;
+//     const popup = window.open(
+//       '/login',
+//       'loginPopup',
+//       `width=${width},height=${height},left=${left},top=${top}`
+//     );
+//     const receiveMessage = (event) => {
+//       if (event.origin !== window.origin) {
+//         return;
+//       }
+
+//       const { type, data } = event.data;
+
+//       if (type === 'login') {
+//         setUsername(data.username);
+//         setPassword(data.password);
+//         popup.close();
+//       }
+//     };
+
+//     window.addEventListener('message', receiveMessage, false);
+//   };
+  
+  
+
+//   return (
+//     <form className="signup-form" onSubmit={handleSubmit}>
+//         <div>
+//             <label htmlFor="username">Username:</label>
+//              <input type="text" id="username" value={username} onChange={handleUsernameChange} required />
+//         </div>
+//         <div>
+//             <label htmlFor="password">Password:</label>
+//              <input type="password" id="password" value={password} onChange={handlePasswordChange} required />
+//         </div>
+//         <div>
+//         <button type="submit">Sign up</button>
+//         </div>
+//         <div className="home-footer">
+//                 <p>Already have an account? <a onClick={handlePopup}>Login</a></p>
+//         </div>
+//     </form>
+//   );
+// }
+// function LoginPopup() {
+//   const [username, setUsername] = useState('');
+//   const [password, setPassword] = useState('');
+
+//   const handleLogin = () => {
+//     window.opener.postMessage(
+//       {
+//         type: 'login',
+//         data: { username, password },
+//       },
+//       window.origin
+//     );
+//   };
+//   return (
+//     <div>
+//       <input
+//         type="text"
+//         placeholder="Username"
+//         value={username}
+//         onChange={(e) => setUsername(e.target.value)}
+//       />
+//       <input
+//         type="password"
+//         placeholder="Password"
+//         value={password}
+//         onChange={(e) => setPassword(e.target.value)}
+//       />
+//       <button onClick={handleLogin}>Log In</button>
+//     </div>
+//   );
+// }
+
+
+// export default SignupForm;
