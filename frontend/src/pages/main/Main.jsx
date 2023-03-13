988bff648cac586caff7b5d960b9b9be5f68e8f1
Eric Duong <eduong@arizona.edu>
2023-03-12 19:14:46 -0700

    3 columns

--- frontend/src/pages/main/Main.jsx
+++ frontend/src/pages/main/Main.jsx
@@ -1,8 +1,22 @@
 import React from "react";
+import './main.scss';
+import TagsPannel from "./tagspannel/TagsPannel";
+import RecipesIngredients from "./recipesingredients/RecipesIngredients";
+import GroceryList from "./grocerylist/GroceryList";
 
 function Main() {
   return (
-    <div>This is the main page</div>
+    <div class="container">
+      <div class="tag-pannel">
+        <TagsPannel />
+      </div>
+      <div class="recipes-ingredients">
+        <RecipesIngredients />
+      </div>
+      <div class="grocery-list">
+        <GroceryList />
+      </div>
+    </div>
   )
 }
 
