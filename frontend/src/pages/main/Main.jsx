6084bf0180f3240ad39d38e7d97f738e923cc851
Eric Duong <eduong@arizona.edu>
2023-03-12 19:29:57 -0700

    phone can only see grocery list

--- frontend/src/pages/main/Main.jsx
+++ frontend/src/pages/main/Main.jsx
@@ -1,23 +1,42 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import './main.scss';
 import TagsPannel from "./tagspannel/TagsPannel";
 import RecipesIngredients from "./recipesingredients/RecipesIngredients";
 import GroceryList from "./grocerylist/GroceryList";
 
 function Main() {
-  return (
-    <div class="container">
-      <div class="tag-pannel">
-        <TagsPannel />
-      </div>
-      <div class="recipes-ingredients">
-        <RecipesIngredients />
-      </div>
-      <div class="grocery-list">
-        <GroceryList />
-      </div>
-    </div>
+  const [matches, setMatches] = useState(
+    window.matchMedia("(min-width: 768px)").matches
   )
+
+  useEffect(() => {
+    window
+    .matchMedia("(min_width: 768px)")
+    .addEventListener('change', e => setMatches(e.matches));
+  }, []);
+
+  return (
+    <div id="root">
+      {matches && (
+        <div className="container">
+          <div className="tag-pannel">
+            <TagsPannel />
+          </div>
+          <div className="recipes-ingredients">
+            <RecipesIngredients />
+          </div>
+          <div className="grocery-list">
+            <GroceryList />
+          </div>
+        </div>
+      )}
+      {!matches && (
+        <div className="grocery-list">
+          <GroceryList />
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Main;
