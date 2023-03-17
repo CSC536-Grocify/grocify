587d05a420aa40614b23f15f2709752a49673b83
Eric Duong <86935179+eduongAZ@users.noreply.github.com>
2023-03-16 19:11:17 -0700

    CSC536-79-mobile-view-navigation-bar (#22)
    * Navigation bar mobile
    
    * Fix scrolling in mobile outside column
    
    * remove device checker listener when not needed

--- frontend/src/pages/main/Main.jsx
+++ frontend/src/pages/main/Main.jsx
@@ -3,21 +3,49 @@ import './main.scss';
 import TagsPannel from "./tagspannel/TagsPannel";
 import RecipesIngredients from "./recipesingredients/RecipesIngredients";
 import GroceryList from "./grocerylist/GroceryList";
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import KitchenIcon from '@mui/icons-material/Kitchen';
+import EggIcon from '@mui/icons-material/Egg';
+import RestaurantIcon from '@mui/icons-material/Restaurant';
 
 function Main() {
   const [matches, setMatches] = useState(
     window.matchMedia("(min-width: 768px)").matches
   );
 
+  // Set matches to true if device is PC, else false (i.e., device is mobile)
   useEffect(() => {
-    window
-    .matchMedia("(min_width: 768px)")
-    .addEventListener('change', e => setMatches(e.matches));
+    const mediaQueryList = window.matchMedia('(min-width: 768px)');
+    const listener = (e) => setMatches(e.matches);
+
+    // Attach the listener to the MediaQueryList
+    mediaQueryList.addEventListener('change', listener);
+
+    // Clean up the listener when the component is unmounted
+    return () => {
+      mediaQueryList.removeEventListener('change', listener);
+    };
   }, []);
+
+  const [bottomNavigationValue, setBottomNavigationValue] = useState(2);
+
+  const renderColumn = () => {
+    switch (bottomNavigationValue) {
+      case 0:
+        return <TagsPannel />;
+      case 1:
+        return <RecipesIngredients />;
+      case 2:
+        return <GroceryList />;
+      default:
+        return <GroceryList />;
+    }
+  };
 
   return (
     <div id="root">
-      {matches && (
+      {matches ? (
         <div className="container">
           <div className="column">
             <TagsPannel />
@@ -29,11 +57,25 @@ function Main() {
             <GroceryList />
           </div>
         </div>
-      )}
-      {!matches && (
-        <div className="container">
-          <div className="column">
-            <GroceryList />
+      ) : (
+        <div className="mobile-container">
+          <div className="container">
+            <div className="mobile-column">
+              {renderColumn()}
+            </div>
+          </div>
+          <div className="bottom-navigation">
+            <BottomNavigation
+              showLabels
+              value={bottomNavigationValue}
+              onChange={(event, newValue) => {
+                setBottomNavigationValue(newValue);
+              }}
+            >
+              <BottomNavigationAction label="Tags" icon={<RestaurantIcon />} />
+              <BottomNavigationAction label="Food" icon={<KitchenIcon />} />
+              <BottomNavigationAction label="Grocery" icon={<EggIcon />} />
+            </BottomNavigation>
           </div>
         </div>
       )}
