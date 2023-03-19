8f7eb3196c5157dd22de85d0bcd51eb37e58155d
CalebUAz <calebshibu@email.arizona.edu>
2023-03-18 18:47:41 -0700

    user model and serializer added

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
-# Create your models here.
+class UserAccountManager(BaseUserManager):
+    def create_user(self, user_email, password=None, **extra_fields):
+        if not user_email:
+            raise ValueError('Users must have an user_email address')
+
+        user_email = self.normalize_user_email(user_email)
+        user = self.model(user_email=user_email, **extra_fields)
+
+        user.set_password(password)
+        user.save()
+
+        return user
+
+class UserAccount(AbstractBaseUser, PermissionsMixin):
+    user_email = models.user_emailField(max_length=255, unique=True)
+    user_name = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+
+    objects = UserAccountManager()
+
+    USERNAME_FIELD = 'user_email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def get_full_name(self):
+        return self.first_name + self.last_name
+
+    def get_short_name(self):
+        return self.first_name
+    
+    def __str__(self):
+        return self.user_email
