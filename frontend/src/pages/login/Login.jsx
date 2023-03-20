da5b06f8f4518e31f5b5360cc0c8a38daf9b3f0d
Eric Duong <eduong@arizona.edu>
2023-03-19 20:44:22 -0700

    Initial set up for authentication

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -4,10 +4,11 @@ import Navbar from './components/Navbar/Navbar';
 
 function Login() {
   return (
-    <>
+    <div>
       <Navbar />
       <Home />
-    </>
+      <Outlet />
+    </div>
   )
 }
 
