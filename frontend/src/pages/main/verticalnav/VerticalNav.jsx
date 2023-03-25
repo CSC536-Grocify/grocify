c103574c98c5081d624bab0e39eed42b9e18d9c4
Eric Duong <eduong@arizona.edu>
2023-03-24 20:39:01 -0700

    Button appears left

--- frontend/src/pages/main/verticalnav/VerticalNav.jsx
+++ frontend/src/pages/main/verticalnav/VerticalNav.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useState } from "react";
 import './VerticalNav.scss';
 import { FaUser } from 'react-icons/fa';
 
 const VerticalNav = () => {
+  const [showLeftButton, setShowLeftButton] = useState(false);
+
+  const handleButtonClick = () => {
+    setShowLeftButton(!showLeftButton);
+  };
+
   return (
     <div className="vertical-nav">
-      <button>
-        <FaUser />
-      </button>
+      <div className="button-container">
+        {showLeftButton && (
+          <button className="left-btn">
+            Log out
+          </button>
+        )}
+        <button className="bottom-btn" onClick={handleButtonClick}>
+          <FaUser />
+        </button>
+      </div>
     </div>
   );
 };
