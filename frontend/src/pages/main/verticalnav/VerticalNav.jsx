d26e43c38fa8b4da9dd9db250e1c8c720cd13030
Eric Duong <eduong@arizona.edu>
2023-03-25 09:08:32 -0700

    logout button action

--- frontend/src/pages/main/verticalnav/VerticalNav.jsx
+++ frontend/src/pages/main/verticalnav/VerticalNav.jsx
@@ -1,18 +1,30 @@
 import React, { useState } from "react";
 import './VerticalNav.scss';
 import { FaUser } from 'react-icons/fa';
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from 'react-redux';
+import { logOut } from '../../../features/auth/authSlice';
 
 const VerticalNav = () => {
   const [showLeftButton, setShowLeftButton] = useState(false);
+  let navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleButtonClick = () => {
     setShowLeftButton(!showLeftButton);
   };
 
+  const handleLogoutButtonClick = (event) => {
+    event.preventDefault();
+
+    dispatch(logOut());
+    navigate('/');
+  }
+
   return (
     <div className="nav-container">
       {showLeftButton && (
-        <button className="left-btn">
+        <button className="left-btn" onClick={handleLogoutButtonClick}>
           Log out
         </button>
       )}
