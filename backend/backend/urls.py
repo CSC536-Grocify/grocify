5735a5262faec8885f9e949e505e821e2a421053
CalebUAz <calebshibu@email.arizona.edu>
2023-03-28 00:30:09 -0700

    Fix recipe view error

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from recipes import recipe_views
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -13,4 +14,8 @@ urlpatterns = [
     path("jwt/create/", TokenObtainPairView.as_view(), name="jwt_create"),
     path("jwt/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("jwt/verify/", TokenVerifyView.as_view(), name="token_verify"),
+    path('recipes', recipe_views.getRecipes),
+    path('recipes/', recipe_views.createRecipe),
+    path('recipes/', recipe_views.updateRecipe),
+    path('recipes/', recipe_views.deleteRecipe),
 ]
