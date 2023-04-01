acc920a5de5e2e232f82d08c13de41106e2c15a5
Harshita Narnoli <122493733+harshunr@users.noreply.github.com>
2023-04-01 00:30:17 -0700

    CSC536-97: Get api added based on user authentication token. (#35)
    Co-authored-by: harshu1803 <harshita.narnoli@alienbrains.in>

--- backend/ingredients/views.py
+++ backend/ingredients/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
-from rest_framework import viewsets
 from .serializers import IngredientSerializer
 from .models import Ingredient
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 
-# Create your views here.
-
-class IngredientView(viewsets.ModelViewSet):
-    serializer_class = IngredientSerializer
-    queryset = Ingredient.objects.all()
+@api_view(['GET']) 
+def getIngredients(request):
+    try:
+        ingredient = Ingredient.objects.filter(user=request.user)
+        serializer = IngredientSerializer(ingredient, many=True)
+        response = {"data": serializer.data, "message": "Ingredients loaded successfully."}
+        return Response(data=response, status=status.HTTP_200_OK)
+    except Exception as e:
+        response = {"data": [], "message": "Ingredients loading issues."}
+        return Response(data=response, status=status.HTTP_404_NOT_FOUND)
