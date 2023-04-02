5f56fa1d465b6583e8163b97d6b56246af00d423
CalebUAz <calebshibu@email.arizona.edu>
2023-04-02 14:56:12 -0700

    update timer to 20 minutes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -159,6 +159,6 @@ REST_FRAMEWORK = {
 AUTH_USER_MODEL = "accounts.User"
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=20),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
