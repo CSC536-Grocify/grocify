4147b7396ed6214957769873597ab21e567be057
Eric Duong <86935179+eduongAZ@users.noreply.github.com>
2023-04-05 11:12:20 -0700

    Grocery list (#49)
    * demo grocery list
    
    * hide user information from tag panel

--- frontend/src/pages/main/grocerylist/GroceryList.jsx
+++ frontend/src/pages/main/grocerylist/GroceryList.jsx
@@ -1,9 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import './GroceryList.scss';
+import { useGetIngredientsQuery } from '../../../features/recipes_ingredients/ingredientsApiSlice';
+
 
 function GroceryList() {
-  return (
-    <div>Grocery List</div>
-  )
+  const [fetchIngredients, setFetchIngredients] = useState(false);
+
+  const {
+    data: ingredientsFromAPI, // rename data to ingredients
+    isLoading,
+    isSuccess,
+    isError,
+    error,
+    refetch
+  } = useGetIngredientsQuery({ skip: !fetchIngredients });
+
+  const handleMakeGroceryListButton = async (event) => {
+    event.preventDefault();
+
+    if (fetchIngredients) {
+      refetch(); // Re-fetch the data when the button is clicked again
+    } else {
+      setFetchIngredients(true);
+    }
+  };
+
+  function handleRemoveButton(event, id) {
+    event.preventDefault();
+    console.log(id);
+  }
+
+  return (isLoading ? <div>Loading...</div> : (
+    <div>
+      <div>Grocery List</div>
+      <button id="button" onClick={handleMakeGroceryListButton}>
+        <span>Make grocery list</span>
+      </button>
+      {fetchIngredients && ingredientsFromAPI && ingredientsFromAPI.data &&
+        <div className="ingredients-container">
+          {ingredientsFromAPI.data.map((ingredient) => (
+            <div className="ingredient-card" key={ingredient.id}>
+              <span className="ingredient-title">{ingredient.name}</span>
+              <button className="remove-button" onClick={(event) => handleRemoveButton(event, ingredient.id)}>
+                Remove
+              </button>
+            </div>
+          ))}
+        </div>
+      }
+    </div>
+  ));
 }
 
 export default GroceryList;
