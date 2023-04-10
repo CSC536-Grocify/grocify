d9ff83d0b6479f9faa10edd81b9ef082738fca9a
Eric Duong <86935179+eduongAZ@users.noreply.github.com>
2023-04-09 23:00:01 -0700

    only render app if authenticated (#51)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,8 @@
-// import logo from './logo.svg';
 import './App.css';
-// import axios from 'axios';
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Main from './pages/main/Main';
+import ProtectedRoutes from './pages/main/ProtectedRoutes';
 import Login from './pages/login/Login';
 import Signup from './pages/login/Signup';
 import PageNotFound from './pages/error/PageNotFound';
@@ -11,54 +10,20 @@ import Recipesdropdown from './pages/main/recipesingredients/RecipesDropDown';
 import Ingredientsdropdown from './pages/main/recipesingredients/IngredientsDropDown';
 
 
-
-// const api = axios.create({
-//   baseURL: process.env.REACT_APP_ENV && process.env.REACT_APP_ENV === 'production' ? 
-//     process.env.REACT_APP_REMOTE_API_URL : process.env.REACT_APP_LOCAL_API_URL,
-// });
-//don't change 3lines above
-
 function App() {
-  // console.log("REACT_APP_ENV=".concat(process.env.REACT_APP_ENV));
-  // const [person, setPerson] = React.useState({});
-  // React.useEffect(() => {
-  //   async function fetchData() {
-  //     const request = await api.get('/');
-  //     setPerson(request.data);
-  //     return request;
-  //   }
-  //   fetchData();
-  // }, []);
-
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Signup />} />
-        <Route path="/main" element={<Main />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/recipe_edit" element={<Recipesdropdown />} />
-        <Route path="/ingredient_edit" element={<Ingredientsdropdown />} />
-        <Route path="*" element={<PageNotFound />} />
-        <Route path="*" element={<PageNotFound />} />
+        <Route path="/*" element={<ProtectedRoutes />}>
+          <Route path="main" element={<Main />} />
+          <Route path="recipe_edit" element={<Recipesdropdown />} />
+          <Route path="ingredient_edit" element={<Ingredientsdropdown />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Route>
       </Routes>
     </Router>
-
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Hi, I am <code>{person.name}</code>. I am <code>{person.age}</code> years old.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
