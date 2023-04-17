a2f594f669e593a7d04a9ee102ee4ac424c3d86d
Eric Duong <eduong@arizona.edu>
2023-04-17 15:46:00 -0700

    fetch tags from API

--- frontend/src/pages/main/tagspannel/TagsPannel.jsx
+++ frontend/src/pages/main/tagspannel/TagsPannel.jsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useGetTagsQuery } from '../../../features/tags/tagsApiSlice';
+import { useLocation } from "react-router-dom";
 
 
 function TagsPannel() {
-  return (
-    <div>Tags panel</div>
-  )
+  const {
+    data: tagsFromAPI, // rename data to tags
+    isLoading,
+    refetch
+  } = useGetTagsQuery();
+  const location = useLocation();
+
+  useEffect(() => {
+    refetch();
+  }, [location, refetch]);
+
+  return (isLoading ? <div>Loading...</div> : (
+    <div>
+      <div>Tags panel</div>
+      <div>
+        {tagsFromAPI.data.map((tag) => (
+          <div key={tag.id}>
+            <span>{tag.name}</span>
+            <ul>
+              {tag.recipes.map((recipe) => (
+                <li key={recipe.id}>{recipe.title}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
+      </div>
+    </div>
+  ));
 }
 
 export default TagsPannel;
