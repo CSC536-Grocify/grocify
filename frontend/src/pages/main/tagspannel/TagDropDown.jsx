7ffda4b853ca395b8946e9e2f92a4a74bca0f55d
Mary <57932176+Mary-Eskandari@users.noreply.github.com>
2023-04-22 07:22:44 -0700

    Design front (#66)
    * add new changes
    
    * new changes
    
    * adding + button into tag pannel and adding rightclick option for Edit and Remove
    
    * new changes
    
    * add plus button into tag and change the remove and edit button to righclick
    
    * chnged
    
    * on touch
    
    * change
    
    * new changs
    
    * edit button on click, delete in edit
    
    * associate recipe to tag
    
    ---------
    
    Co-authored-by: Eric Duong <86935179+eduongAZ@users.noreply.github.com>
    Co-authored-by: Eric Duong <eduong@arizona.edu>

---
+++ frontend/src/pages/main/tagspannel/TagDropDown.jsx
@@ -0,0 +1,137 @@
+import React, { useState, useEffect } from "react";
+import { useGetRecipesQuery } from '../../../features/recipes_ingredients/recipesApiSlice';
+import './TagDropDown.scss';
+import {
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    TextField,
+    Autocomplete,
+} from "@mui/material";
+
+
+function TagDropDown({ open, handleClose, handleSave, handleDelete, currentTagInfo = null }) {
+    const [selectedRecipes, setSelectedRecipes] = useState([]);
+    const [name, setName] = useState('');
+    const {
+        data: recipesFromAPI,
+        isRecipesLoading
+    } = useGetRecipesQuery();
+
+    useEffect(() => {
+        if (currentTagInfo) {
+            setName(currentTagInfo.name);
+            setSelectedRecipes(currentTagInfo.recipes);
+        }
+    }, [currentTagInfo]);
+
+    const handleNameChange = (event) => {
+        setName(event.target.value);
+    };
+
+    function resetData() {
+        setName("");
+    }
+
+    const handleSaveClick = async () => {
+        const newTagInfo = {
+            name: name,
+            id: null,
+            recipe_ids: selectedRecipes.map((recipe) => recipe.id)
+        };
+
+        if (currentTagInfo && currentTagInfo.hasOwnProperty('id')) {
+            newTagInfo.id = currentTagInfo.id;
+        }
+
+        await handleSave(newTagInfo);
+        resetData();
+        handleClose();
+    };
+
+    const handleCloseClick = () => {
+        resetData();
+        handleClose();
+    };
+
+    const handleDeleteClick = async () => {
+        await handleDelete(currentTagInfo.id);
+        resetData();
+        handleClose();
+    };
+
+    const addRecipeToSelectedRecipes = (newRecipe) => {
+        const recipeExists = selectedRecipes.some((recipe) => recipe.id === newRecipe.id);
+
+        if (!recipeExists) {
+            setSelectedRecipes([...selectedRecipes, newRecipe]);
+        }
+    };
+
+    const handleSelectionChange = (event, newRecipe) => {
+        const recipeExistsInAPI = recipesFromAPI?.data.some((recipe) => recipe.id === newRecipe.id);
+
+        if (recipeExistsInAPI) {
+            addRecipeToSelectedRecipes(newRecipe);
+        }
+    };
+
+    const removeRecipe = (id) => {
+        const updatedSelectedRecipes = selectedRecipes.filter((recipe) => recipe.id !== id);
+        setSelectedRecipes(updatedSelectedRecipes);
+    };
+
+    return (isRecipesLoading ? <div>Loading...</div> :
+        <Dialog open={open} onClose={handleCloseClick}>
+            <DialogTitle>{currentTagInfo ? "Edit Tag" : "Add New Tag"}</DialogTitle>
+            <DialogContent>
+                <TextField
+                    autoFocus
+                    margin="dense"
+                    label="Tag Name"
+                    fullWidth
+                    value={name}
+                    onChange={handleNameChange}
+                />
+                <Autocomplete
+                    // when the component is initially rendered, the data from the API might not be available yet
+                    options={recipesFromAPI?.data || []}
+                    getOptionLabel={(option) => typeof option === 'string' ? option : option.title}
+                    onChange={handleSelectionChange}
+                    renderInput={(params) => (
+                        <TextField
+                            {...params}
+                            label="Recipe Search"
+                            variant="outlined"
+                        />
+                    )}
+                />
+                <div className="selected-recipes-container">
+                    {selectedRecipes.map((recipe) => (
+                        <div className="selected-recipes" key={recipe.id} >
+                            <span className="selected_remove_item">{recipe.title}</span>
+                            <button className="selected_remove" onClick={() => removeRecipe(recipe.id)}>Remove</button>
+                        </div>
+                    ))}
+                </div>
+            </DialogContent>
+            <DialogActions>
+                {currentTagInfo && currentTagInfo.hasOwnProperty('id') && (
+                    <Button onClick={handleDeleteClick} color="primary">
+                        Delete
+                    </Button>
+                )}
+                <Button onClick={handleCloseClick} color="primary">
+                    Cancel
+                </Button>
+                <Button onClick={handleSaveClick} color="primary">
+                    Save
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
+}
+
+export default TagDropDown;
--- frontend/src/pages/main/tagspannel/TagsPannel.jsx
+++ frontend/src/pages/main/tagspannel/TagsPannel.jsx
@@ -1,32 +1,112 @@
-import React, { useEffect } from "react";
-import { useGetTagsQuery } from '../../../features/tags/tagsApiSlice';
+import React, { useState, useEffect } from "react";
+import { useDeleteTagMutation, useGetTagsQuery } from '../../../features/tags/tagsApiSlice';
+import { useCreateTagMutation, useUpdateTagMutation } from '../../../features/tags/tagsApiSlice';
 import { useLocation } from "react-router-dom";
+import TagDropDown from './TagDropDown';
+import './TagsPannel.scss';
 
 
-function TagsPannel() {
+function TagsPanel() {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [modelOpenArgument, setModalOpenArgument] = useState(null);
+  const [createTag, { isCreateLoading }] = useCreateTagMutation();
+  const [updateTag, { isUpdateLoading }] = useUpdateTagMutation();
+  const [deleteTagAPI, { isDeleteTagLoading }] = useDeleteTagMutation();
   const {
-    data: tagsFromAPI, // rename data to tags
+    data: tagsFromAPI,
     isLoading,
     refetch
   } = useGetTagsQuery();
-  const location = useLocation();
 
+  const location = useLocation();
   useEffect(() => {
     refetch();
   }, [location, refetch]);
 
-  return (isLoading ? <div>Loading...</div> : (
+  const handleCreateNew = (event) => {
+    event.preventDefault();
+
+    const modalInformation = {
+      tag_info: null
+    };
+
+    setModalOpenArgument(modalInformation);
+  };
+
+  // Opening modal when modal argument is set
+  useEffect(() => {
+    if (modelOpenArgument) {
+      setModalOpen(true);
+    }
+  }, [modelOpenArgument]);
+
+  const handleModalClose = () => {
+    setModalOpenArgument(null);
+    setModalOpen(false);
+  };
+
+  const handleSaveTag = async (newTagInfo) => {
+    try {
+      if (newTagInfo.id === null) {
+        await createTag({
+          name: newTagInfo.name,
+          recipe_ids: newTagInfo.recipe_ids.join(',')
+        }).unwrap();
+      } else {
+        await updateTag({
+          id: newTagInfo.id,
+          name: newTagInfo.name,
+          recipe_ids: newTagInfo.recipe_ids.join(',')
+        }).unwrap();
+      }
+      await refetch();
+      setModalOpenArgument(null);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleDeleteTag = async (id) => {
+    try {
+      await deleteTagAPI({ id }).unwrap();
+      await refetch();
+      setModalOpenArgument(null);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleTagClick = (event, tag) => {
+    event.preventDefault();
+
+    const modalInformation = {
+      tag_info: tag
+    };
+
+    setModalOpenArgument(modalInformation);
+  };
+
+  return (isLoading || isCreateLoading || isUpdateLoading || isDeleteTagLoading ? <div>Loading...</div> : (
     <div>
       <div>Tags panel</div>
-      <div>
+      <button id="button" className="add-btn" onClick={(event) => handleCreateNew(event)}>
+        <span>+</span>
+      </button>
+      <TagDropDown
+        open={modalOpen}
+        handleClose={handleModalClose}
+        handleSave={handleSaveTag}
+        handleDelete={handleDeleteTag}
+        currentTagInfo={modelOpenArgument ? modelOpenArgument.tag_info : null}
+      />
+      <div className="tags-container">
         {tagsFromAPI.data.map((tag) => (
-          <div key={tag.id}>
-            <span>{tag.name}</span>
-            <ul>
-              {tag.recipes.map((recipe) => (
-                <li key={recipe.id}>{recipe.title}</li>
-              ))}
-            </ul>
+          <div
+            className="tag-card"
+            key={tag.id}
+            onClick={(event) => handleTagClick(event, tag)}
+          >
+            <span className="tag-title">{tag.name}</span>
           </div>
         ))}
       </div>
@@ -34,4 +114,5 @@ function TagsPannel() {
   ));
 }
 
-export default TagsPannel;
+export default TagsPanel;
+
