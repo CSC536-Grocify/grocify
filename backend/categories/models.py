e5cdcca458542174b1d7368b35f1ed23b32323c2
Caleb Jones Shibu <90938130+CalebUAz@users.noreply.github.com>
2023-04-23 00:31:33 -0700

    CRUD operations for categories (#71)

---
+++ backend/categories/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import User
+from accounts.models import User
+from recipes.models import Ingredient
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=120, unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    ingredient = models.ManyToManyField(Ingredient, through='ingredient_categories.IngredientCategory')
+
+    def _str_(self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        self.name = self.name.lower()
+        super(Category, self).save(*args, **kwargs)
+    
