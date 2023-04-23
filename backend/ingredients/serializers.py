a960ba9484357d7ede881a67d2dbac01737412de
Caleb Jones Shibu <90938130+CalebUAz@users.noreply.github.com>
2023-04-23 01:10:47 -0700

    CSC536-135: updated API for ingredient create and edit (#72)

--- backend/ingredients/serializers.py
+++ backend/ingredients/serializers.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
 from .models import Ingredient
+from categories.serializers import CategorySerializer
 from backend import CapitalizedCharField
 
 class IngredientSerializer(serializers.ModelSerializer):
     name = CapitalizedCharField(max_length=120)
+    categories = serializers.SerializerMethodField()
+
     class Meta:
         model = Ingredient
-        fields = ('id', 'name', 'notes')
+        fields = ('id', 'name', 'notes', 'categories')
+
+    def get_categories(self, obj):
+        categories = obj.categories.all()
+        return CategorySerializer(categories, many=True).data
