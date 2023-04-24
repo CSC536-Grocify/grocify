5a299c201adf26967f68bead9f749e50b2f6bd1e
Eric Duong <86935179+eduongAZ@users.noreply.github.com>
2023-04-23 21:36:25 -0700

    CSC536-131-display-grocery-list-information (#76)
    * make grocery list
    
    * Create custom item in grocery list

--- frontend/src/pages/main/tagspannel/TagsPannel.jsx
+++ frontend/src/pages/main/tagspannel/TagsPannel.jsx
@@ -1,123 +1,127 @@
 import React, { useState, useEffect } from "react";
-import { useDeleteTagMutation, useGetTagsQuery } from '../../../features/tags/tagsApiSlice';
-import { useCreateTagMutation, useUpdateTagMutation } from '../../../features/tags/tagsApiSlice';
+import {
+    useDeleteTagMutation,
+    useGetTagsQuery,
+    useCreateTagMutation,
+    useUpdateTagMutation
+} from '../../../features/tags/tagsApiSlice';
 import { useLocation } from "react-router-dom";
 import TagDropDown from './TagDropDown';
 import './TagsPannel.scss';
 
 
 function TagsPanel() {
-  const [modalOpen, setModalOpen] = useState(false);
-  const [modelOpenArgument, setModalOpenArgument] = useState(null);
-  const [createTag, { isCreateLoading }] = useCreateTagMutation();
-  const [updateTag, { isUpdateLoading }] = useUpdateTagMutation();
-  const [deleteTagAPI, { isDeleteTagLoading }] = useDeleteTagMutation();
-  const {
-    data: tagsFromAPI,
-    isLoading,
-    refetch
-  } = useGetTagsQuery();
+    const [modalOpen, setModalOpen] = useState(false);
+    const [modelOpenArgument, setModalOpenArgument] = useState(null);
+    const [createTag, { isCreateLoading }] = useCreateTagMutation();
+    const [updateTag, { isUpdateLoading }] = useUpdateTagMutation();
+    const [deleteTagAPI, { isDeleteTagLoading }] = useDeleteTagMutation();
+    const {
+        data: tagsFromAPI,
+        isLoading,
+        refetch
+    } = useGetTagsQuery();
 
-  const location = useLocation();
-  useEffect(() => {
-    refetch();
-  }, [location, refetch]);
+    const location = useLocation();
+    useEffect(() => {
+        refetch();
+    }, [location, refetch]);
 
-  const handleCreateNew = (event) => {
-    event.preventDefault();
+    const handleCreateNew = (event) => {
+        event.preventDefault();
 
-    const modalInformation = {
-      tag_info: null
+        const modalInformation = {
+            tag_info: null
+        };
+
+        setModalOpenArgument(modalInformation);
     };
 
-    setModalOpenArgument(modalInformation);
-  };
+    // Opening modal when modal argument is set
+    useEffect(() => {
+        if (modelOpenArgument) {
+            setModalOpen(true);
+        }
+    }, [modelOpenArgument]);
 
-  // Opening modal when modal argument is set
-  useEffect(() => {
-    if (modelOpenArgument) {
-      setModalOpen(true);
-    }
-  }, [modelOpenArgument]);
-
-  const handleModalClose = () => {
-    setModalOpenArgument(null);
-    setModalOpen(false);
-  };
-
-  const handleSaveTag = async (newTagInfo) => {
-    try {
-      if (newTagInfo.id === null) {
-        await createTag({
-          name: newTagInfo.name,
-          recipe_ids: newTagInfo.recipe_ids.join(',')
-        }).unwrap();
-      } else {
-        await updateTag({
-          id: newTagInfo.id,
-          name: newTagInfo.name,
-          recipe_ids: newTagInfo.recipe_ids.join(',')
-        }).unwrap();
-      }
-      await refetch();
-      setModalOpenArgument(null);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const handleDeleteTag = async (id) => {
-    try {
-      await deleteTagAPI({ id }).unwrap();
-      await refetch();
-      setModalOpenArgument(null);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const handleTagClick = (event, tag) => {
-    event.preventDefault();
-
-    const modalInformation = {
-      tag_info: tag
+    const handleModalClose = () => {
+        setModalOpenArgument(null);
+        setModalOpen(false);
     };
 
-    setModalOpenArgument(modalInformation);
-  };
+    const handleSaveTag = async (newTagInfo) => {
+        try {
+            if (newTagInfo.id === null) {
+                await createTag({
+                    name: newTagInfo.name,
+                    recipe_ids: newTagInfo.recipe_ids.join(',')
+                }).unwrap();
+            } else {
+                await updateTag({
+                    id: newTagInfo.id,
+                    name: newTagInfo.name,
+                    recipe_ids: newTagInfo.recipe_ids.join(',')
+                }).unwrap();
+            }
+            await refetch();
+            setModalOpenArgument(null);
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
-  return (isLoading || isCreateLoading || isUpdateLoading || isDeleteTagLoading ? <div>Loading...</div> : (
-    <div>
-      <div className="tags-tab">TAGS PANEL</div>
-      <button id="button" className="add-btn" onClick={(event) => handleCreateNew(event)}>
-        <span>+</span>
-      </button>
-      <TagDropDown
-        open={modalOpen}
-        handleClose={handleModalClose}
-        handleSave={handleSaveTag}
-        handleDelete={handleDeleteTag}
-        currentTagInfo={modelOpenArgument ? modelOpenArgument.tag_info : null}
-      />
-      <div className="tags-container">
-        {tagsFromAPI.data.map((tag) => (
-          <div
-            className="tag-card"
-            key={tag.id}
-            onClick={(event) => handleTagClick(event, tag)}
-          >
-            <span className="tag-title">{tag.name}</span>
-            <ul>
-              {tag.recipes.map((recipe) => (
-                <li className="recipes-list" key={recipe.id}>{recipe.title}</li>
-              ))}
-            </ul>
-          </div>
-          
-        ))}
-      </div>
-    </div>
-  ));
+    const handleDeleteTag = async (id) => {
+        try {
+            await deleteTagAPI({ id }).unwrap();
+            await refetch();
+            setModalOpenArgument(null);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const handleTagClick = (event, tag) => {
+        event.preventDefault();
+
+        const modalInformation = {
+            tag_info: tag
+        };
+
+        setModalOpenArgument(modalInformation);
+    };
+
+    return (isLoading || isCreateLoading || isUpdateLoading || isDeleteTagLoading ? <div>Loading...</div> : (
+        <div>
+            <div className="tags-tab">TAGS PANEL</div>
+            <button id="button" className="add-btn" onClick={(event) => handleCreateNew(event)}>
+                <span>+</span>
+            </button>
+            <TagDropDown
+                open={modalOpen}
+                handleClose={handleModalClose}
+                handleSave={handleSaveTag}
+                handleDelete={handleDeleteTag}
+                currentTagInfo={modelOpenArgument ? modelOpenArgument.tag_info : null}
+            />
+            <div className="tags-container">
+                {tagsFromAPI.data.map((tag) => (
+                    <div
+                        className="tag-card"
+                        key={tag.id}
+                        onClick={(event) => handleTagClick(event, tag)}
+                    >
+                        <span className="tag-title">{tag.name}</span>
+                        <ul>
+                            {tag.recipes.map((recipe) => (
+                                <li className="recipes-list" key={recipe.id}>{recipe.title}</li>
+                            ))}
+                        </ul>
+                    </div>
+
+                ))}
+            </div>
+        </div>
+    ));
 }
 
 export default TagsPanel;
