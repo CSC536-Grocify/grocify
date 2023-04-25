a2b1c56e6e6c406cbb313b156cccfee06de6e80e
CalebUAz <calebshibu@email.arizona.edu>
2023-04-25 16:09:36 -0700

    unique constraint error fix

--- backend/grocery_lists/models.py
+++ backend/grocery_lists/models.py
@@ -6,7 +6,7 @@ from ingredients.models import Ingredient
 
 # Create your models here.
 class GroceryList(models.Model):
-    name = models.CharField(max_length=120, unique=True)
+    name = models.CharField(max_length=120)
     notes = models.TextField(null=True)
     quantity = models.FloatField(null=True)
     measurement = models.ForeignKey(Measurement, on_delete=models.CASCADE, null=True)
@@ -19,3 +19,7 @@ class GroceryList(models.Model):
     def save(self, *args, **kwargs):
         self.name = self.name.lower()
         super(GroceryList, self).save(*args, **kwargs)
+
+    class Meta:
+        ordering = ['name']
+        unique_together = ['name', 'user']
