5447416f1ad95d795978e13571d9777ffb1d0ac1
Caleb Jones Shibu <90938130+CalebUAz@users.noreply.github.com>
2023-04-25 17:28:38 -0700

    unique constraint error fix (#83)

--- backend/ingredients/models.py
+++ backend/ingredients/models.py
@@ -4,7 +4,7 @@ from accounts.models import User
 
 # Create your models here.
 class Ingredient(models.Model):
-    name = models.CharField(max_length=120, unique=True)
+    name = models.CharField(max_length=120)
     notes = models.TextField(null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     categories = models.ManyToManyField('categories.Category', through='ingredient_categories.IngredientCategory', related_name='ingredients')
@@ -15,3 +15,7 @@ class Ingredient(models.Model):
     def save(self, *args, **kwargs):
         self.name = self.name.lower()
         super(Ingredient, self).save(*args, **kwargs)
+
+    class Meta:
+        ordering = ['name']
+        unique_together = ['name', 'user']
